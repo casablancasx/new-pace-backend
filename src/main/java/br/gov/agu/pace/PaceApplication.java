5003c2d3e5dde5d5e5e5d7da26a49133f1dc4660
DaniloChaves <danchavesds@gmail.com>
2025-12-10 11:25:16 -0300

    commit inicial

---
+++ src/main/java/br/gov/agu/pace/PaceApplication.java
@@ -0,0 +1,13 @@
+package br.gov.agu.pace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaceApplication.class, args);
+	}
+
+}
