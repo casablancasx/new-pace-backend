e6164f8a14d99e27dfddf8f7d5549cdd256ae90f
DaniloChaves <danchavesds@gmail.com>
2025-12-17 11:56:31 -0300

    refact: refactor AssuntoController and add OpenAPI configuration

--- src/main/java/br/gov/agu/pace/domain/assunto/AssuntoController.java
+++ src/main/java/br/gov/agu/pace/domain/assunto/AssuntoController.java
@@ -1,7 +1,5 @@
 package br.gov.agu.pace.domain.assunto;
 
-import br.gov.agu.pace.domain.sala.SalaEntity;
-import br.gov.agu.pace.domain.sala.SalaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -11,22 +9,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sala")
+@RequestMapping("/assunto")
 @RequiredArgsConstructor
 public class AssuntoController {
 
     private final AssuntoService assuntoService;
 
-
     @GetMapping
-    public ResponseEntity<Page<AssuntoEntity>> listarSalas(
+    public ResponseEntity<Page<AssuntoEntity>> listarAssuntos(
             @RequestParam(required = false) String nome,
             @RequestParam(required = false) String orderBy,
             @RequestParam(defaultValue = "asc") String sort,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
 
-        var assuntos = assuntoService.listarAssuntos(page, size, nome, orderBy,sort);
+        var assuntos = assuntoService.listarAssuntos(page, size, nome, orderBy, sort);
         return ResponseEntity.ok(assuntos);
     }
 }
