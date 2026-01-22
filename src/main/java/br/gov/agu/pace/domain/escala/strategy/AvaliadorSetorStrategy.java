c5bdc410becdb07cb04e1c6ec8aa67b42e330829
DaniloChaves <danchavesds@gmail.com>
2026-01-22 01:00:35 -0300

    refact: enhance AvaliadorSetorStrategy and PautistaSetorStrategy to dynamically retrieve setor IDs based on Subnucleo and ClasseJudicial attributes

--- src/main/java/br/gov/agu/pace/domain/escala/strategy/AvaliadorSetorStrategy.java
+++ src/main/java/br/gov/agu/pace/domain/escala/strategy/AvaliadorSetorStrategy.java
@@ -2,25 +2,50 @@ package br.gov.agu.pace.domain.escala.strategy;
 
 import br.gov.agu.pace.domain.audiencia.entity.AudienciaEntity;
 import br.gov.agu.pace.domain.enums.Subnucleo;
+import br.gov.agu.pace.domain.setor.SetorEntity;
+import br.gov.agu.pace.domain.unidade.UnidadeEntity;
 import br.gov.agu.pace.domain.user.UserEntity;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
-public class AvaliadorSetorStrategy implements SetorStrategy{
-
-    private static final Map<Subnucleo, Long> MAP_SETORES = Map.of(
-            Subnucleo.ESEAS, 8057746L,
-            Subnucleo.EBI, 8057765L,
-            Subnucleo.ERU,8054825L
-    );
-
+public class AvaliadorSetorStrategy implements SetorStrategy {
 
 
     @Override
     public Long getSetorId(UserEntity user, AudienciaEntity audiencia) {
-        return MAP_SETORES.get(audiencia.getSubnucleo());
+
+        List<UnidadeEntity> unidades = user.getSetores().stream()
+                .map(SetorEntity::getUnidade)
+                .toList();
+
+
+        Long setorId = null;
+
+        for (UnidadeEntity unidade : unidades) {
+            Subnucleo subnucleoDaUnidade = extrairSubnucleo(unidade.getSigla());
+            assert subnucleoDaUnidade != null;
+            if (subnucleoDaUnidade.equals(audiencia.getSubnucleo())) {
+                setorId = unidade.getUnidadeId();
+                break;
+            }
+        }
+
+        return setorId;
+
+    }
+
+    private Subnucleo extrairSubnucleo(String sigla) {
+        Pattern pattern = Pattern.compile("(EBI|ESEAS|ERU)\\d*", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(sigla);
+
+        if (matcher.find()) {
+            return Subnucleo.getSubnucleo(matcher.group(1));
+        }
+        return null;
     }
 
 }
--- src/main/java/br/gov/agu/pace/domain/escala/strategy/PautistaSetorStrategy.java
+++ src/main/java/br/gov/agu/pace/domain/escala/strategy/PautistaSetorStrategy.java
@@ -3,37 +3,66 @@ package br.gov.agu.pace.domain.escala.strategy;
 import br.gov.agu.pace.domain.audiencia.entity.AudienciaEntity;
 import br.gov.agu.pace.domain.enums.ClasseJudicial;
 import br.gov.agu.pace.domain.enums.Subnucleo;
+import br.gov.agu.pace.domain.setor.SetorEntity;
+import br.gov.agu.pace.domain.unidade.UnidadeEntity;
 import br.gov.agu.pace.domain.user.UserEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Component
 public class PautistaSetorStrategy implements SetorStrategy{
 
 
-    private static final Map<Subnucleo, Long> JEF_SETORES = Map.of(
-            Subnucleo.ESEAS, 8057731L,
-            Subnucleo.EBI, 8057767L,
-            Subnucleo.ERU,8054827L
-    );
 
-    private static final Map<Subnucleo, Long> COMUM_SETORES = Map.of(
-            Subnucleo.ESEAS, 8057730L,
-            Subnucleo.EBI, 8057766L,
-            Subnucleo.ERU,8059088L
-    );
 
     @Override
     public Long getSetorId(UserEntity user, AudienciaEntity audiencia) {
 
 
-        if (audiencia.getClasseJudicial().equals(ClasseJudicial.JEF)){
-            return JEF_SETORES.get(audiencia.getSubnucleo());
-        }else{
-            return COMUM_SETORES.get(audiencia.getSubnucleo());
-        }
+        List<UnidadeEntity> unidades = user.getSetores().stream()
+                .map(SetorEntity::getUnidade)
+                .toList();
 
+        Long setorId = null;
+
+        for (UnidadeEntity unidade : unidades) {
+
+            Subnucleo subnucleoDaUnidade = extrairSubnucleo(unidade.getSigla());
+            ClasseJudicial classeDaUnidade = extrairClasse(unidade.getNome());
+            assert subnucleoDaUnidade != null;
+
+            if(audiencia.getSubnucleo().equals(subnucleoDaUnidade) && audiencia.getClasseJudicial().equals(classeDaUnidade)){
+                setorId = unidade.getUnidadeId();
+                break;
+            }
+        }
+        return setorId;
+
+    }
+
+
+
+    private Subnucleo extrairSubnucleo(String sigla) {
+        Pattern pattern = Pattern.compile("(EBI|ESEAS|ERU)\\d*", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(sigla);
+
+        if (matcher.find()) {
+            return Subnucleo.getSubnucleo(matcher.group(1));
+        }
+        return null;
+    }
+
+    private ClasseJudicial extrairClasse(String nome) {
+
+        if (nome.contains("JEF") || nome.contains("JUIZADOS")){
+            return ClasseJudicial.JEF;
+        }else {
+            return ClasseJudicial.COMUM;
+        }
     }
 }
